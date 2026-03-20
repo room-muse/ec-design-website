5b1a84cd7490c75d45ff4273adc8271ae3671c11
adrao1 <adrao@berkeley.edu>
2026-03-20 19:44:22 -0400

    add parent url

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Demo() {
   const params = new URLSearchParams(window.location.search);
   const company = params.get("company") || "";
-  const src = `https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html${company ? "?company=" + company : ""}`;
+  const src = `https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html?company=${company}&rm_parentUrl=${encodeURIComponent(window.location.href)}`;
   return (
     <iframe src={src} style={{ width: "100%", height: "100vh", border: "none" }} />
   );
