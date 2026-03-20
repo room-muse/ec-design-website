a2019ec1e8c3fb84365eb9a20b422587b711ebbc
adrao1 <adrao@berkeley.edu>
2026-03-20 19:33:54 -0400

    add company param to iframe

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function Demo() {
-    return (
-      <iframe
-        src="https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html"
-        style={{ width: "100%", height: "100vh", border: "none" }}
-      />
-    );
-  }
+  const params = new URLSearchParams(window.location.search);
+  const company = params.get("company") || "";
+  const src = `https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html${company ? "?company=" + company : ""}`;
+  return (
+    <iframe src={src} style={{ width: "100%", height: "100vh", border: "none" }} />
+  );
+}
