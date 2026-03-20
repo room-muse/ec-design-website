fa8f49465406b4f8994e38de5d97e54a85e5a027
adrao1 <adrao@berkeley.edu>
2026-03-19 20:21:25 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Hero } from "./components/Hero";
 import { Value } from "./components/Value";
 import { HowItWorks } from "./components/HowItWorks";
 import { Integration } from "./components/Integration";
 import { LeadForm } from "./components/LeadForm";
 import { Footer } from "./components/Footer";
+import Demo from "./pages/Demo";
 
-export default function App() {
+function Home() {
   return (
     <div className="min-h-screen bg-[#FAF8F3]">
       <Hero />
@@ -15,5 +18,16 @@ export default function App() {
       <LeadForm />
       <Footer />
     </div>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Demo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Demo() {
+    return (
+      <iframe
+        src="https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html"
+        style={{ width: "100%", height: "100vh", border: "none" }}
+      />
+    );
+  }
