cbde43111c68e5f6ab150a5104acd84e8026379c
adrao1 <adrao@berkeley.edu>
2026-03-20 19:48:50 -0400

    forward rm_arId

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Demo() {
   const params = new URLSearchParams(window.location.search);
   const company = params.get("company") || "";
-  const src = `https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html?company=${company}&rm_parentUrl=${encodeURIComponent(window.location.href)}`;
+  const arId = params.get("rm_arId") || "";
+  const parentUrl = encodeURIComponent(window.location.href);
+  
+  let src = `https://furniture-store-frontend-sooty.vercel.app/index-with-ar.html?company=${company}&rm_parentUrl=${parentUrl}`;
+  if (arId) src += `&rm_arId=${arId}`;
+
   return (
     <iframe src={src} style={{ width: "100%", height: "100vh", border: "none" }} />
   );
