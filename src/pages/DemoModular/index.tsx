79176ca6f768bb02300029bc8f54b2c546b28f3b
adrao1 <adrao@berkeley.edu>
2026-04-03 00:05:20 -0400

    add components

--- src/pages/DemoModular/index.tsx
+++ src/pages/DemoModular/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import TopBar from "./components/TopBar";
+import Canvas from "./components/Canvas";
+import ColorPicker from "./components/ColorPicker";
+import PieceStrip from "./components/PieceStrip";
 
 export default function DemoModular() {
-    return <div>DemoModular</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <TopBar />
+      <div style={{ flex: 1 }}>
+        <Canvas />
+      </div>
+      <ColorPicker />
+      <PieceStrip />
+    </div>
+  );
 }
