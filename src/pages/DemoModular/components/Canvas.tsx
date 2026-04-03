1d90d81c36a3f8efd6f4dea3e9c4f7d24112eb7b
adrao1 <adrao@berkeley.edu>
2026-04-03 01:29:03 -0400

    canvas

--- src/pages/DemoModular/components/Canvas.tsx
+++ src/pages/DemoModular/components/Canvas.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Canvas() {
-  return <div className="h-full w-full bg-gray-100">Canvas</div>;
+  return (
+    <div className="flex-1 w-full bg-gray-50 flex items-center justify-center">
+      <div className="text-center">
+        <p className="text-lg font-medium text-gray-700">
+          Start building your sectional
+        </p>
+        <p className="mt-2 text-sm text-gray-500">
+          Select a piece from the strip below
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/pages/DemoModular/index.tsx
+++ src/pages/DemoModular/index.tsx
@@ -3,15 +3,16 @@ import TopBar from "./components/TopBar";
 import Canvas from "./components/Canvas";
 import ColorPicker from "./components/ColorPicker";
 import PieceStrip from "./components/PieceStrip";
+import { Piece } from "../../types/piece";
 
 export default function DemoModular() {
-  const [pieces, setPieces] = React.useState([]);
+  const [pieces, setPieces] = React.useState<Piece[]>([]);
   const [totalPrice, setTotalPrice] = React.useState(0);
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex h-screen flex-col ">
       <TopBar pieceCount={pieces.length} totalPrice={totalPrice} />
-      <div className="flex-1 min-h-0">
+      <div className="flex flex-1 min-h-0">
         <Canvas />
       </div>
       <PieceStrip />
